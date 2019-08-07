726d55868f174daf4e4646505d055618ba12125f
robjo82 <rjrdutg@gmail.com>
2019-08-07 16:24:24 +0200

    Initialisation des paramètres, premières urls et création de l'application 'registration'

--- MaisSemence82/MaisSemence82/urls.py
+++ MaisSemence82/MaisSemence82/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from . import views
+from django.conf.urls import include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.index, name = 'index'),
+    path('registration/', include('registration.urls')),
 ]
