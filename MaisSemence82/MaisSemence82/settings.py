d2ef26f2a1798ae8229529bed66ed94650679a7c
robjo82 <rjrdutg@gmail.com>
2019-08-07 10:00:43 +0200

    Configuration initiale de settings

--- MaisSemence82/MaisSemence82/settings.py
+++ MaisSemence82/MaisSemence82/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'MaisSemence82.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -103,9 +103,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'fr-fr'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Paris'
 
 USE_I18N = True
 
@@ -118,3 +118,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static"),
+)
+
+MEDIA_ROOT = '/media/'
+
+MEDIA_URL = '/media/'
+
+"""LOGIN_REDIRECT_URL = 'connect'
+
+LOGIN_URL = 'login'"""
